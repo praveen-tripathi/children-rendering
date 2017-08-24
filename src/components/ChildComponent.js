a76301512bdc04b74d71a513ed4de9f207ddc8cb
Praveen <praveen.tripathi@benefitfocus.com>
2017-08-24 15:11:56 +0530

    small change

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -10,7 +10,8 @@ export default class ChildComponent extends React.Component {
         return (
            <div>
                 <h2>Child 1</h2>
-                <h2>Child 2</h2>    
+                <h2>Child 2</h2>
+                <h2>Child 3</h2>
            </div>
         )
     }
